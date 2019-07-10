a78142874265b19eebc5eb6579d70a914eec22a4
nkottary <kottarynishanth54@gmail.com>
2019-07-10 13:29:28 +0530

    Address comments

--- src/forges/GitLab/tags.jl
+++ src/forges/GitLab/tags.jl
@@ -8,9 +8,11 @@ end
     release::Release
     name::String
     target::String
-    message::Union{Nothing, String}
+    message::String
 end
 
 endpoint(::GitLabAPI, ::typeof(get_tags), project::Integer) =
     Endpoint(:GET, "/projects/$project/repository/tags")
+endpoint(::GitLabAPI, ::typeof(get_tags), owner::AStr, repo::AStr) =
+    Endpoint(:GET, "/projects/$(encode(owner, repo))/repository/tags")
 into(::GitLabAPI, ::typeof(get_tags)) = Vector{Tag}
