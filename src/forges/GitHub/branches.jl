1e4ac7bebdf78d1c4268ca2c753fc240e19e1fc5
Fernando Chorney <fernando.chorney@invenia.ca>
2021-09-02 13:50:53 -0500

    Add extra branch functions
    - get_branches
    - delete_branch
    - Add an "owner/repo" version of get_pull_requests for GitLab

--- src/forges/GitHub/branches.jl
+++ src/forges/GitHub/branches.jl
@@ -25,3 +25,29 @@ end
 endpoint(::GitHubAPI, ::typeof(get_branch), owner::AStr, repo::AStr, branch::AStr) =
     Endpoint(:GET, "/repos/$owner/$repo/branches/$branch")
 into(::GitHubAPI, ::typeof(get_branch)) = Branch
+
+# Get Repository Branches
+# https://docs.github.com/en/rest/reference/repos#list-branches
+function endpoint(
+    ::GitHubAPI,
+    ::typeof(get_branches),
+    owner::AStr,
+    repo::AStr,
+)
+    return EndPoint(:GET, "repos/$owner/$repo/branches")
+end
+into(::GitHubAPI, ::typeof(get_branches)) = Vector{Branch}
+
+# Delete Branch (Delete Reference)
+# https://docs.github.com/en/rest/reference/git#delete-a-reference
+# The :ref in the URL must be formatted as heads/<branch name> for branches
+function endpoint(
+    ::GitHubAPI,
+    ::typeof(delete_branch),
+    owner::AStr,
+    repo::AStr,
+    branch::AStr,
+)
+    return Endpoint(:DELETE, "/repos/$owner/$repo/git/refs/heads/$branch")
+end
+postprocessor(::GitHubAPI, ::typeof(delete_branch)) = DoNothing
